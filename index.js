c3758bc075b8d0903794e4fe8d644d9069228600
trong.phamtranduc@gmail.com <trong.phamtranduc@gmail.com>
2024-12-14 14:46:33 +0700

    update file

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
